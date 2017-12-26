885da7616768bcb94ce829adc539cc89591958ac
Joseph <josembi@gmail.com>
2017-12-26 14:52:17 +0100

    Add Simple-form & View files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,43 @@
 class TasksController < ApplicationController
+	before_action :find_task, only: [:show, :edit, :update, :destroy ]
+
 	def index
 		
+	end
+
+	def show
+		
+	end
+
+	def new
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.new(tasks_params)
+		if @task.save 
+			redirect_to @task
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+
+	private
+
+	def tasks_params
+		params.require(:task).permit(:title, :description, :company, :url)
+	end
+
+	def find_tasks
+		@task = Task.find(params[:id])
 	end
 end
