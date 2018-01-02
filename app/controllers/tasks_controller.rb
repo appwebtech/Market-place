166d62b4a7ff0a5090aa9bb56de6a1c2d26454d6
Joseph <josembi@gmail.com>
2018-01-02 10:00:58 +0100

    First 2018 Commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
 	private
 
 	def tasks_params
-		params.require(:task).permit(:title, :description, :company, :url)
+		params.require(:task).permit(:title, :description, :company, :url, :category_id)
 	end
 
 	def find_task
